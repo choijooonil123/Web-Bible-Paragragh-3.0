7b8fa1dcf14012df24e4595636a8945965767c2d
choijooonil123 <choijooonil123@gmail.com>
2025-10-29 00:54:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -603,7 +603,7 @@ function openSingleDocEditor(kind){
   modalWrap.setAttribute('aria-hidden','false');
   modalFooterNew.style.display = 'none';
 
-  sermonTitle.value = doc.title || '';
+  // sermonTitle.value = doc.title || '';
   setBodyHTML(doc.body || '');
 
   sermonEditor.dataset.editing = '';
